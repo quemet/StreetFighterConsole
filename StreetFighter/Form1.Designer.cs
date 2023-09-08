439ddc0603f096507c4c42b20eced8805c5a5ab1
quemet <129742514+quemet@users.noreply.github.com>
2023-09-08 16:31:40 +0200

    Add files via upload

--- StreetFighter/Form1.Designer.cs
+++ StreetFighter/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 25F);
+            this.label1.Location = new System.Drawing.Point(234, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Your Player";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(139, 138);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(487, 264);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
